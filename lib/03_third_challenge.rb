c021c671b5630a1d7e6279af41c19ecc108fa014
Donat Pllana <donat.pllana@outlook.com>
2020-06-07 04:33:25 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -16,7 +16,7 @@ def third_challenge
         #your key/value pairs here
         :name=>"Romeo",
         :age=>"15",
-        :status:"Alive"
+        :status=>"Alive"
         },
       :hero_friends => [
         {
