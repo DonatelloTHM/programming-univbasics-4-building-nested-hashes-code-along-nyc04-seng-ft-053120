27d05a6e1d7bb9a3d5390457d3dbfe299c08ca12
Donat Pllana <donat.pllana@outlook.com>
2020-06-07 04:39:40 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -29,7 +29,7 @@ def fourth_challenge
       {
         :name=>"Nurse",
         :age=>"44",
-        :attitude=>"confused"
+        :attitude=>"worried"
       }
         #your hashes here!
       ]
